571db22b5b77a1e1ede3366df18cbf8aa0327ec6
MichaeTav <m.tavera@hotmail.com>
2022-05-25 18:53:27 -0700

    conveted /login to mui

---
+++ client/src/components/forms/LoginForm.js
@@ -0,0 +1,94 @@
+import React, { useContext, useState } from "react";
+import { useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import { useNavigate } from "react-router-dom";
+import { Box, TextField, Button, Stack, Typography } from "@mui/material";
+
+import { AuthContext } from "../../context/auth";
+import { useForm } from "../../util/hooks";
+
+function LoginForm() {
+  const context = useContext(AuthContext);
+
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState({});
+
+  const { onChange, onSubmit, values } = useForm(loginUserCallback, {
+    username: "",
+    password: "",
+  });
+
+  const [loginUser, { loading }] = useMutation(LOGIN_USER, {
+    update(_, { data: { login: userData } }) {
+      context.login(userData);
+      navigate("/");
+    },
+    onError(err) {
+      setErrors(err.graphQLErrors[0].extensions.exception);
+    },
+    variables: values,
+  });
+
+  function loginUserCallback() {
+    loginUser();
+  }
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "25ch" },
+        margin: "10px",
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <Stack
+        direction="column"
+        justifyContent="space-evenly"
+        alignItems="center"
+        spacing={0}
+      >
+        <TextField
+          name="username"
+          label="Username"
+          type="text"
+          variant="standard"
+          value={values.username}
+          onChange={onChange}
+        />
+        <TextField
+          name="password"
+          label="Password"
+          type="password"
+          variant="standard"
+          value={values.password}
+          onChange={onChange}
+        />
+        <Box sx={{ marginTop: "20px" }}>
+          <Button variant="contained" type="submit" onClick={onSubmit}>
+            Submit
+          </Button>
+        </Box>
+      </Stack>
+    </Box>
+  );
+}
+
+const LOGIN_USER = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      user {
+        id
+        email
+        username
+        password
+        roles {
+          name
+        }
+      }
+      token
+    }
+  }
+`;
+
+export default LoginForm;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,86 +1,15 @@
-import React, { useContext, useState } from "react";
-import { Button, Form } from "semantic-ui-react";
-import { useMutation } from "@apollo/react-hooks";
-import gql from "graphql-tag";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import { Box, Typography } from "@mui/material";
 
-import { AuthContext } from "../context/auth";
-import { useForm } from "../util/hooks";
+import LoginForm from "../components/forms/LoginForm";
 
 function Login(props) {
-  const context = useContext(AuthContext);
-
-  const navigate = useNavigate();
-  const [errors, setErrors] = useState({});
-
-  const { onChange, onSubmit, values } = useForm(loginUserCallback, {
-    username: "",
-    password: "",
-  });
-
-  const [loginUser, { loading }] = useMutation(LOGIN_USER, {
-    update(_, { data: { login: userData } }) {
-      context.login(userData);
-      navigate("/");
-    },
-    onError(err) {
-      setErrors(err.graphQLErrors[0].extensions.exception);
-    },
-    variables: values,
-  });
-
-  function loginUserCallback() {
-    loginUser();
-  }
-
   return (
-    <div className="form-container">
-      <Form onSubmit={onSubmit} noValidate className={loading ? "loading" : ""}>
-        <h1>Login</h1>
-        <Form.Input
-          label="Username"
-          placeholder="Username.."
-          name="username"
-          type="text"
-          value={values.username}
-          onChange={onChange}
-        />
-        <Form.Input
-          label="Password"
-          placeholder="Password.."
-          name="password"
-          type="password"
-          value={values.password}
-          onChange={onChange}
-        />
-        <Button type="submit" primary>
-          Login
-        </Button>
-      </Form>
-      {Object.keys(errors).length > 0 && (
-        <div className="ui error message">
-          <ul className="list">{Object.values(errors)}</ul>
-        </div>
-      )}
-    </div>
+    <Box sx={{ marginTop: "10px" }} alignItems="center">
+      <Typography variant="h3">Login</Typography>
+      <LoginForm />
+    </Box>
   );
 }
-
-const LOGIN_USER = gql`
-  mutation login($username: String!, $password: String!) {
-    login(username: $username, password: $password) {
-      user {
-        id
-        email
-        username
-        password
-        roles {
-          name
-        }
-      }
-      token
-    }
-  }
-`;
 
 export default Login;
